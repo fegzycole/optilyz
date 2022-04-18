0004d8c7bb547168453b5cfd1306959a5b518b9f
Ferguson Iyara <fergusoniyara@gmail.com>
2022-04-18 02:17:49 +0100

    chore: add router wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
 import BoilerplateTheme from "./theme";
 import "./index.css";
 
@@ -10,8 +11,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={BoilerplateTheme}>
-      <div />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={BoilerplateTheme}>
+        <div />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
