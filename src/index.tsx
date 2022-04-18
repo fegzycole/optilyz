7105367b89f6842fbcd323b53a9823dd7ea3a8a1
Ferguson Iyara <fergusoniyara@gmail.com>
2022-04-18 03:22:14 +0100

    chore: make a change

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import BoilerplateTheme from './theme';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import BoilerplateTheme from './theme';
+import App from './containers/App';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
                 <ThemeProvider theme={BoilerplateTheme}>
-                    <div />
+                    <App />
                 </ThemeProvider>
             </BrowserRouter>
         </Provider>
