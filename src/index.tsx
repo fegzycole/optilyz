56330bb9e7bf6daab7ac32ff7a9e8b5ca07ef557
Ferguson Iyara <fergusoniyara@gmail.com>
2022-04-18 02:26:00 +0100

    chore: setup redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import BoilerplateTheme from './theme';
 import './index.css';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <ThemeProvider theme={BoilerplateTheme}>
-                <div />
-            </ThemeProvider>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <ThemeProvider theme={BoilerplateTheme}>
+                    <div />
+                </ThemeProvider>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
 );
