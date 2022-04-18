b3f9e64df17b3385b613236d40794e261eec4237
Ferguson Iyara <fergusoniyara@gmail.com>
2022-04-18 02:22:03 +0100

    chore: format files

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter } from "react-router-dom";
-import BoilerplateTheme from "./theme";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter } from 'react-router-dom';
+import BoilerplateTheme from './theme';
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={BoilerplateTheme}>
-        <div />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ThemeProvider theme={BoilerplateTheme}>
+                <div />
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
 );
