04d49e05f52f10395d42286edcb8560c41c39a83
Ferguson Iyara <fergusoniyara@gmail.com>
2022-04-18 03:24:29 +0100

    chore: make a change

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import BoilerplateTheme from './theme';
 import App from './containers/App';
 import store from './store';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
